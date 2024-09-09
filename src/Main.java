30b122923e6f6b59c995f4093ab8f35d2bde6c83
arthurw2027 <arthurw2027@student.cis.edu.hk>
2024-09-09 10:11:38 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -9,8 +9,34 @@
 public class Main {
 
     public static void main(String[] args) {
+System.out.println(roundNegativeValueToNearestInteger(-7.41));
+    }
+    public static int add(int a, int g){
+        return a + g;
+    }
+    public static int add(int c, int d, int e, int f){
+        return add(c, d) + add(e,f);
+    }
+    public static String morningGreeting(String name){
+        return "早上好 " + name;
+    }
+    public static String afternoonGreeting(String Name){
+        return "下午好 " + Name;
+    }
+    public static String triple(String three){
+        return three + three + three;
+    }
+    public static double half(double halves){
+        return halves/2;
+    }
+    public static double roundPositiveValueToNearestInteger(double rounded){
+        return (int) (rounded + 0.5);
+    }
+    public static double roundNegativeValueToNearestInteger(double roundDown){
+        return (int) (roundDown - 0.5);
 
     }
+
 
     // 1. add
     
